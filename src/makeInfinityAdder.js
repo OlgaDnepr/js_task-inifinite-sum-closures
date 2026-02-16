8f681c4f15ab38acee60e97e0119d7b9bde15898
Your Name <cocochoco.story@gmail.com>
2026-02-16 19:50:19 +0200

    solution

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -4,7 +4,23 @@
  * @return {function}
  */
 function makeInfinityAdder() {
-  // write code here
+  let sum = 0;
+
+  return function adder(...args) {
+    if (args.length > 0) {
+      sum += args.reduce((summ, x) => summ + x, 0);
+
+      return adder;
+    }
+
+    if (args.length === 0) {
+      const result = sum;
+
+      sum = 0;
+
+      return result;
+    }
+  };
 }
 
 module.exports = makeInfinityAdder;
